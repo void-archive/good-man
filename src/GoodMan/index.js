91a788c69f6862c3f84dcef114ce5af58039db9f
Ryan (void) <RyanMcKeown82@Gmail.com>
2018-05-17 08:07:08 +0800

    good man is moving up

---
+++ src/GoodMan/index.js
@@ -0,0 +1,11 @@
+class GoodMan {
+    constructor(man) {
+        this.man = man;
+    }
+
+    isAGoodMan(man) {
+        console.log(`${man} is a good man`);
+    }
+}
+
+module.exports = GoodMan;
